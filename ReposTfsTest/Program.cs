dd65403b02ff0226754ce70eee26f7e4054e073c
Dra <dara_r@rambler.ru>
2014-05-07 11:48:04 +0300

    Init

---
+++ ReposTfsTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReposTfsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("end a program");
+            Console.ReadLine();
+        }
+    }
+}
