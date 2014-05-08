9f5ee5845e871ae8e1a74b62e0ad82039a9034db
Dra <dara_r@rambler.ru>
2014-05-08 11:52:27 +0300

    work with events

--- event2/Program.cs
+++ event2/Program.cs
@@ -10,6 +10,54 @@ namespace event2
     {
         static void Main(string[] args)
         {
+            var tc = new TestClass();
+            var h1 = new Handler1();
+            tc.MyEvent += h1.Test;
+            tc.Test();
+
+            Console.WriteLine("end a program");
+            Console.ReadLine();
+        }
+    }
+
+    public class TestClass
+    {
+        private EventHandler _myHandler;
+
+        public event EventHandler MyEvent
+        {
+            add
+            {
+                lock (this)
+                {
+                    _myHandler += value;
+                }
+            }
+            remove
+            {
+                lock (this)
+                {
+                    if (_myHandler != null && value != null)
+                        _myHandler -= value;
+                }
+            }
+        }
+
+        public void Test()
+        {
+            EventHandler handler = _myHandler;
+            if (null != handler)
+            {
+                handler(this, new EventArgs());
+            }
+        }
+    }
+
+    public class Handler1
+    {
+        public void Test(object sender, EventArgs e)
+        {
+            Console.WriteLine("Handler1 test");
         }
     }
 }
